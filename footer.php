7eda7bac19e52c04f513eba05ea98cc6fe95de45
Brenton Horne <brentonhorne77@gmail.com>
2015-10-14 19:00:46 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,8 +8,8 @@
                 </div>
                 <div class="col-md-6 text-right">
                     <small class="text-muted">
-                        <?php printf(__('&copy <strong>%s</strong>', 'Sabayon'), get_bloginfo('name')); ?></strong><br>
-                        <?php printf(__('Theme by %s', 'Sabayon'), '<a href="' . esc_url('https://github.com/fusion809/Sabayon') . '" target="_blank">Brenton</a>'); ?>
+                        <?php printf(__('&copy <strong>%s</strong>', 'sabayon-gitsta'), get_bloginfo('name')); ?></strong><br>
+                        <?php printf(__('Theme by %s', 'sabayon-gitsta'), '<a href="' . esc_url('https://github.com/fusion809/sabayon-gitsta') . '" target="_blank">Brenton Horne</a>'); ?>
                     </small>
                 </div>
             </div>
